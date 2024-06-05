a1fd1dc66b9a3e26e54ca798f0ea9f0240fbb63b
Nguyen Duc Hoang <sunlight4d@gmail.com>
2024-06-05 15:41:20 +0700

    update

---
+++ A2209I/MVC/de01/Models/Klass.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+namespace de01.Models
+{    
+    [Table("Class")]
+    public class Klass
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(100, ErrorMessage = "Class name cannot be longer than 100 characters.")]
+        public string ClassName { get; set; }
+    }
+
+}
