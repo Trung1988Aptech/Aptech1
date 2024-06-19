fb92c43c832ee2ef367872508172d66b2a20ea6d
Nguyen Duc Hoang <sunlight4d@gmail.com>
2024-06-19 15:53:11 +0700

    update

---
+++ A2209I/MVC/de07/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using de07.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace de07.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<Service> Services { get; set; }
+        public DbSet<ServiceCategory> ServiceCategories { get; set; }
+    }
+}
