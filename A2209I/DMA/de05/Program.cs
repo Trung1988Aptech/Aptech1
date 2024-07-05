c30ee12ca5a32e761f1cc8414171dafa023603fb
Nguyen Duc Hoang <sunlight4d@gmail.com>
2024-07-05 15:13:41 +0700

    update

---
+++ A2209I/DMA/de05/Program.cs
@@ -0,0 +1,42 @@
+using de05.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.EntityFrameworkCore.Design;
+
+var builder = WebApplication.CreateBuilder(args);
+/**
+dotnet tool install --global dotnet-ef
+dotnet build
+dotnet ef database update
+dotnet ef migrations add AddSomeEntities --context DataContext
+dotnet ef database update --context DataContext
+
+ */
+// Add services to the container.
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") 
+    ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+
+builder.Services.AddDbContext<DataContext>(options =>
+    options.UseSqlServer(connectionString));
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
