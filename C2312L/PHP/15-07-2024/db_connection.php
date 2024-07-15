dcf63fa1afcb3540b74256d0d980e3610816cf2f
Nguyen Duc Hoang <sunlight4d@gmail.com>
2024-07-15 20:10:52 +0700

    add docker composer

--- C2312L/PHP/15-07-2024/db_connection.php
+++ C2312L/PHP/15-07-2024/db_connection.php
@@ -8,9 +8,11 @@ write a function to do this inside php file
 //define("SERVER_NAME", "localhost");
 const SERVER_NAME = "localhost";
 const USERNAME = "root";
-const PASSWORD = "";
+const PASSWORD = "Abc123456789";//docker container
+//const PASSWORD = "";
 const DATABASE_NAME = "c2312l";
-const PORT = 3306;
+//const PORT = 3306;
+const PORT = 3307;//docker container
 
 
 function connectToDatabase() {
@@ -27,6 +29,31 @@ function connectToDatabase() {
         die("Connection failed: " . $e->getMessage());
     }
 }
+
+/*
+function connectToDatabase() {
+    $serverName = SERVER_NAME;
+    $username = USERNAME;
+    $password = PASSWORD;
+    $databaseName = DATABASE_NAME;
+    $port = PORT;
+    
+    // Create connection
+    $mysqli = new mysqli($serverName, $username, $password, $databaseName, $port);
+
+    // Check connection
+    if ($mysqli->connect_error) {
+        die("Connection failed: " . $mysqli->connect_error);
+    }
+
+    // Set charset to utf8
+    if (!$mysqli->set_charset("utf8")) {
+        die("Error loading character set utf8: " . $mysqli->error);
+    }
+
+    return $mysqli;
+}
+*/
 
 // Example of using the function
 $pdo = connectToDatabase();
